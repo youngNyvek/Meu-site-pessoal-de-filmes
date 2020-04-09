25fd091f57329f01cf7bc9aba5b17016bcf0b06a
Kevin Faria <kevintxd73@gmail.com>
2020-04-09 20:09:37 -0300

    Pagina filmes
    Dei uma breve estilizada na pagina aonde vão se encontrar os filmes

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+// const invisibles = document.querySelectorAll(".invisible")
+
+// const img = document.querySelector(".card_image-container img")
+
+
+// for (invisible of invisibles){
+
+// invisible.addEventListener("mouseover", function(){
+
+//     img.classList.add("img-hover")
+// })
+
+// invisible.addEventListener("mouseout", function(){
+
+//     img.classList.remove("img-hover")
+// })
+
+// }
